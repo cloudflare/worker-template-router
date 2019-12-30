c99a703196ebef3b390793224c673d64da575cff
Nikz Jon <nikhiljohn1010@gmail.com>
2019-12-31 03:57:25 +0530

    Reworked for HTML specific serving

--- index.js
+++ index.js
@@ -1,30 +1,70 @@
-const Router = require('./router')
+const Router = require('./lib/router')
+const Render = require('./lib/render')
+Render.subPath = '/html' // Base path
+Render.assetPath = '/public' // Path of assets to load
+Render.title = "Hello World!"
 
-/**
- * Example of how router can be used in an application
- *  */
+/* Custom GET Paths and their Handlers */
+const paths = [
+  ['/', () => htmlLoader()],
+  ['/json', () => jsonLoader()],
+  ['/robot.txt', () => Render.res("User-agent: *\nAllow: /html", 'txt')],
+  ['/secret', () => Render.error("Access Restricted", 403)],
+  ['/cf', () => Render.res("https://cloudflare.com", 'redirect')],
+  ['/request', req => Render.res(JSON.stringify(req), 'json')],
+  ['/q', req => {
+    const params = (new URL(req.url)).searchParams
+    return Render.res(JSON.stringify({id: params.get('id'), name: params.get('name')}), 'json')
+  }]
+]
+
+/* Event Listener */
 addEventListener('fetch', event => {
-    event.respondWith(handleRequest(event.request))
+  event.respondWith(handleRequest(event.request))
 })
 
-function handler(request) {
-    const init = {
-        headers: { 'content-type': 'application/json' },
-    }
-    const body = JSON.stringify({ some: 'json' })
-    return new Response(body, init)
+/* Custom Loaders */
+async function htmlLoader() {
+  try {
+    const template = await Render.loadTemplate()
+    const html = await Render.loadContent(template)
+    return Render.res(html, 'html')
+  } catch (err) {
+    return Render.error(err)
+  }
 }
 
+function jsonLoader() {
+  const json = {
+    test: "Hello World!",
+    array: [1, 2, 3, 4, 5],
+    object: {
+      newArray: ['a', 'b', 'c', 'd']
+    }
+  }
+  return Render.res(JSON.stringify(json), 'json')
+}
+
+/* Request Handler */
 async function handleRequest(request) {
-    const r = new Router("/api/v1") // The base path is passed as parameter
-    // Replace with the approriate paths and handlers
-    r.get('.*/bar', () => new Response('responding for /bar'))
-    r.get('.*/foo', req => handler(req))
-    r.post('.*/foo.*', req => handler(req))
-    r.get('/demos/router/foo', req => fetch(req)) // return the response from the origin
-
-    r.get('/', () => new Response('Hello worker!')) // return a default message for the root route
-
-    const resp = await r.route(request)
-    return resp
+  try {
+    const route = new Router(Render.subPath)
+    const path = (new URL(request.url)).pathname
+    if (path.includes(Render.assetPath)) {
+      await Render.loadAssets(route)
+    } else {
+      /* Loading GET Paths */
+      await Render.loadPaths(route, paths)
+      /* Loading Other Paths */
+      route.post('/', () => Render.res())
+      route.post('.*/foo', () => Render.res("foo")) // Accepts RegEx as path
+    }
+    const result = await route.resolve(request)
+    if (result) {
+      return result.handler(request)
+    }
+    return Render.error("Page Not Found", 404)
+  } catch (err) {
+    return Render.error(err)
+  }
 }
---
+++ public/js/script.js
@@ -0,0 +1,1 @@
+//alert("Script loaded successfully")
